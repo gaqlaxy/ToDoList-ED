b899958bdf0cbd30c32520b00b8e41e5a8cd9d1e
gaqlaxy <v4vripper@gmail.com>
2024-07-26 16:51:44 +0530

    modified

--- script.js
+++ script.js
@@ -16,22 +16,26 @@ function addingTodo(event){
     // creating a div 
     let todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
+
     // creating a li 
     let litodo = document.createElement("li");
     litodo.innerText = "Hey";
     litodo.classList.add("todo-item");
     todoDiv.appendChild(litodo);
     console.log(todoDiv);
+
     //checked button
     let checkedBtn = document.createElement("button");
     checkedBtn.innerHTML= '<i class="fas fa-check"></i>';
     checkedBtn.classList.add("checked-btn");
     todoDiv.appendChild(checkedBtn);
+
     // delete button 
     let delBtn = document.createElement("button");
     delBtn.innerHTML= '<i class="fas fa-trash"></i>';
     delBtn.classList.add("checked-btn");
     todoDiv.appendChild(delBtn);
-    // append to ul 
+
+    // appending to ul 
     todoList.appendChild(todoDiv);
 }
