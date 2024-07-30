159567e6b04aec7bfc00e6b66afd0f1254c9f7a2
gaqlaxy <v4vripper@gmail.com>
2024-07-30 14:24:58 +0530

    Renamed

--- script.js
+++ script.js
@@ -1,60 +1,64 @@
-// Selecting 
+// Selecting
 
 let inputValue = document.querySelector(".todo-input");
 let button = document.querySelector(".submit");
 let todoList = document.querySelector(".todo-list");
 
-// Event Listening 
+// Event Listening
 
 button.addEventListener("click", addingTodo);
 todoList.addEventListener("click", deleteCheck);
 
-// Functions 
+// Functions
 
-function addingTodo(event){
-    event.preventDefault(); //form wont be refreshing with this
-  
-    // creating a div 
-    let todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
+function addingTodo(event) {
+  event.preventDefault(); //form wont be refreshing with this
 
-    // creating a li 
-    let litodo = document.createElement("li");
-    litodo.innerText = inputValue.value;
-    litodo.classList.add("todo-item");
-    todoDiv.appendChild(litodo);
-    // console.log(todoDiv);
+  // creating a div
+  let todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
 
-    //checked button
-    let checkedBtn = document.createElement("button");
-    checkedBtn.innerHTML= '<i class="fas fa-check"></i>';
-    checkedBtn.classList.add("checked-btn");
-    todoDiv.appendChild(checkedBtn);
+  // creating a li
+  let litodo = document.createElement("li");
+  litodo.innerText = inputValue.value;
+  litodo.classList.add("todo-item");
+  todoDiv.appendChild(litodo);
+  // console.log(todoDiv);
 
-    // delete button 
-    let delBtn = document.createElement("button");
-    delBtn.innerHTML= '<i class="fas fa-trash"></i>';
-    delBtn.classList.add("trash-btn");
-    todoDiv.appendChild(delBtn);
+  //checked button
+  let checkedBtn = document.createElement("button");
+  checkedBtn.innerHTML = '<i class="fas fa-check"></i>';
+  checkedBtn.classList.add("checked-btn");
+  todoDiv.appendChild(checkedBtn);
 
-    // appending to ul 
-    todoList.appendChild(todoDiv);
-    // Clear todo inputValue 
-    inputValue.value="";
+  // delete button
+  let delBtn = document.createElement("button");
+  delBtn.innerHTML = '<i class="fas fa-trash"></i>';
+  delBtn.classList.add("trash-btn");
+  todoDiv.appendChild(delBtn);
+
+  // appending to ul
+  todoList.appendChild(todoDiv);
+  // Clear todo inputValue
+  inputValue.value = "";
 }
 
-function deleteCheck(e){
-    let item = e.target;
-    // deleting todo 
-    if(item.classList[0] === "trash-btn"){
-        let todos = item.parentElement;
-        todos.remove();
-    }
+function deleteCheck(e) {
+  let item = e.target;
+  // deleting todo
+  if (item.classList[0] === "trash-btn") {
+    let todos = item.parentElement;
+    todos.classList.add("fall");
+    // todos.remove();
+    todos.addEventListener("transitionend", function () {
+      todos.remove();
+    });
+  }
 
-    //check mark
+  //check mark
 
-    if(item.classList[0] === "checked-btn"){
-        let todos = item.parentElement;
-        todos.classList.toggle("completed");
-    }
+  if (item.classList[0] === "checked-btn") {
+    let todos = item.parentElement;
+    todos.classList.toggle("completed");
+  }
 }
